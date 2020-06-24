17ae3fdb64c26b1905a5608d4cbbb73407d75256
Wilfred Hughes <me@wilfred.me.uk>
2020-06-24 00:25:21 -0700

    Add a crude lisp parser

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
-let Parsimmon = require("parsimmon");
+let P = require("parsimmon");
 
-const parser = Parsimmon.string("foo").map(function(x) {
-  return x + "bar";
+const parser = P.createLanguage({
+  Value: function(r) {
+    return P.alt(r.Number, r.Symbol, r.String, r.List);
+  },
+  Number: function() {
+    return P.regexp(/[0-9]+/).map(Number);
+  },
+  Symbol: function() {
+    return P.regexp(/[a-z]+/);
+  },
+  String: function() {
+    return P.regexp(/"((?:\\.|.)*?)"/, 1);
+  },
+  List: function(r) {
+    return P.string("(")
+      .then(r.Value.sepBy(r._))
+      .skip(P.string(")"));
+  },
+  _: function() {
+    return P.optWhitespace;
+  }
 });
 
 const inputNode = document.getElementById("input");
 
 setInterval(function() {
-  const result = parser.parse(inputNode.value);
+  const result = parser.Value.parse(inputNode.value);
   document.getElementById("output").textContent = JSON.stringify(result);
 }, 1000);

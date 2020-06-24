b3a93f973403cdb51f3a3eefa61f6757b8d06672
Wilfred Hughes <me@wilfred.me.uk>
2020-06-24 00:17:29 -0700

    Hook up a trivial parser

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("howdy");
+let Parsimmon = require("parsimmon");
+
+const parser = Parsimmon.string("foo").map(function(x) {
+  return x + "bar";
+});
+
+const inputNode = document.getElementById("input");
+
+setInterval(function() {
+  const result = parser.parse(inputNode.value);
+  document.getElementById("output").textContent = JSON.stringify(result);
+}, 1000);

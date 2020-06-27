27a00881132342a0116db6d7073d5f6177ab73d5
Wilfred Hughes <me@wilfred.me.uk>
2020-06-26 23:05:51 -0700

    Install and configure eslint

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true
+  },
+  extends: "eslint:recommended",
+  rules: {
+    "linebreak-style": ["error", "unix"],
+    "no-alert": "warn",
+    "no-console": ["warn", { allow: ["warn", "error"] }],
+    "no-debugger": "warn",
+    "no-dupe-else-if": "warn",
+    "no-unused-expressions": "warn",
+    "no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+    "no-useless-call": "warn",
+    "no-useless-computed-key": "warn",
+    "no-useless-concat": "warn",
+    "no-useless-rename": "warn",
+    "no-useless-return": "warn",
+    "no-var": "error",
+    "object-shorthand": ["warn", "properties"],
+    "prefer-arrow-callback": "error",
+    "prefer-const": "error",
+    "sort-imports": "warn",
+    semi: ["error", "always"]
+  }
+};

f3b8485bf4aa936bc18da9e9c99ee417b890a1db
Wilfred Hughes <me@wilfred.me.uk>
2020-07-04 18:51:11 -0700

    Lint react hooks

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,7 +10,11 @@ module.exports = {
     es6: true,
     node: true
   },
-  extends: ["eslint:recommended", "plugin:react/recommended"],
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended"
+  ],
   rules: {
     "linebreak-style": ["error", "unix"],
     "no-alert": "warn",

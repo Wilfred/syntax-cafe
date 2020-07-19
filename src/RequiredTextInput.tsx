45e9df650769e056024e4e20d4e2511132fd7041
Wilfred Hughes <me@wilfred.me.uk>
2020-07-19 16:00:54 -0700

    Port RequiredTextInput to typescript

--- src/RequiredTextInput.tsx
+++ src/RequiredTextInput.tsx
@@ -2,9 +2,14 @@ import React, { useState } from "react";
 
 // Ensure that inputs are not empty, and only call onChange for valid
 // inputs. Highlight invalid inputs.
-export default function RequiredTextInput(props) {
-  const minLength = props.minLength || 1;
-  const [actualValue, setActualValue] = useState(props.value);
+const RequiredTextInput: React.FC<{
+  minLength: number;
+  value: string;
+  placeholder: string;
+  onChange: (_: string) => void;
+}> = ({ minLength, value, placeholder, onChange }) => {
+  minLength = minLength || 1;
+  const [actualValue, setActualValue] = useState(value);
 
   let className = "input";
   if (actualValue.length < minLength) {
@@ -15,15 +20,16 @@ export default function RequiredTextInput(props) {
     <input
       className={className}
       type="text"
-      placeholder={props.placeholder}
+      placeholder={placeholder}
       value={actualValue}
-      onChange={e => {
+      onChange={(e) => {
         const value = e.target.value;
         setActualValue(value);
         if (value.length >= minLength) {
-          props.onChange(value);
+          onChange(value);
         }
       }}
     />
   );
-}
+};
+export default RequiredTextInput;

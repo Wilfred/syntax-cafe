fc92f2f18548f9c5a92bc74f65982da5f5db58fe
Wilfred Hughes <me@wilfred.me.uk>
2020-07-18 19:00:11 -0700

    Remove twemoji script, it doesn't have any effect

--- src/emoji.js
+++
@@ -1,7 +0,0 @@
-// Set the size of the rendered Emojis
-// This can be set to 16x16, 36x36, or 72x72
-twemoji.size = "36x36";
-
-// Parse the document body and
-// insert <img> tags in place of Unicode Emojis
-twemoji.parse(document.body);

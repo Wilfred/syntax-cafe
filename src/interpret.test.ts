05c96dc05b1756bb8c18fb9c427149cf1134d550
Wilfred Hughes <me@wilfred.me.uk>
2020-08-30 18:06:23 -0700

    Refactor evaluation test suite to use a single function

--- src/interpret.test.ts
+++ src/interpret.test.ts
@@ -5,10 +5,12 @@ import { buildParser } from "./parsing";
 
 const PARSER = buildParser(DEFAULT_LANG_OPTS, "do");
 
-function parseAndRun(src: string): Context {
+function expectEvalTo(src: string, value: any): void {
   const parseResult = PARSER.Program.parse(src);
   if (parseResult.status) {
-    return run(parseResult.value, DEFAULT_LANG_OPTS);
+    const ctx = run(parseResult.value, DEFAULT_LANG_OPTS);
+    expect(ctx.error).toBeNull();
+    expect(ctx.result.value).toBe(value);
   } else {
     expect(parseResult.status).toBe(true);
     // Jest will have terminated on the previous line, make tsc happy.
@@ -16,8 +18,17 @@ function parseAndRun(src: string): Context {
   }
 }
 
-function expectError(ctx: Context): void {
-  expect(ctx.error).not.toBeNull();
+function expectEvalError(src: string): Context {
+  const parseResult = PARSER.Program.parse(src);
+  if (parseResult.status) {
+    const ctx = run(parseResult.value, DEFAULT_LANG_OPTS);
+    expect(ctx.error).not.toBeNull();
+    return ctx;
+  } else {
+    expect(parseResult.status).toBe(true);
+    // Jest will have terminated on the previous line, make tsc happy.
+    throw Error("Assertion failed");
+  }
 }
 
 function expectResult(ctx: Context, value: any): void {
@@ -26,151 +37,102 @@ function expectResult(ctx: Context, value: any): void {
 }
 
 test("Boolean literal evaluation", () => {
-  const ctx = parseAndRun("true");
-
-  expectResult(ctx, true);
+  expectEvalTo("true", true);
 });
 
 test("Multiple expressions", () => {
-  const ctx = parseAndRun("true false");
-
-  expectResult(ctx, false);
+  expectEvalTo("true false", false);
 });
 
 test("String literal evaluation", () => {
-  const ctx = parseAndRun('"foo"');
-
-  expectResult(ctx, "foo");
+  expectEvalTo('"foo"', "foo");
 });
 
 describe("add", () => {
   it("should evaluate 1 + 2 as 3", () => {
-    const ctx = parseAndRun("(add 1 2)");
-
-    expectResult(ctx, 3);
+    expectEvalTo("(add 1 2)", 3);
   });
   it("should require numbers", () => {
-    const ctx = parseAndRun('(add 1 "foo")');
-
-    expectError(ctx);
+    expectEvalError('(add 1 "foo")');
   });
 });
 
 test("Unbound variable error", () => {
-  const ctx = parseAndRun("nosuchvar");
-
-  expectError(ctx);
+  const ctx = expectEvalError("nosuchvar");
   expect(ctx.error).toBe("Unbound variable: nosuchvar");
 });
 
 test("Unbound function in argument", () => {
-  // Error should propagate.
-  const ctx = parseAndRun("(add 1 (nosuchfunc))");
-
-  expectError(ctx);
+  expectEvalError("(add 1 (nosuchfunc))");
 });
 
 describe("if", () => {
   it("should evaluate the first expression on true conditions", () => {
-    const ctx = parseAndRun("(if true 2 (foo))");
-
-    expectResult(ctx, 2);
+    expectEvalTo("(if true 2 (foo))", 2);
   });
   it("should evaluate the second expression on false conditions", () => {
-    const ctx = parseAndRun("(if false (foo) 2)");
-
-    expectResult(ctx, 2);
+    expectEvalTo("(if false (foo) 2)", 2);
   });
   it("should error on non boolean inputs", () => {
-    const ctx = parseAndRun("(if 123 1 2)");
-
-    expectError(ctx);
+    expectEvalError("(if 123 1 2)");
   });
 });
 
 test("Call nonexistent function", () => {
-  const ctx = parseAndRun("(foo)");
-
-  expectError(ctx);
+  expectEvalError("(foo)");
 });
 
 test("Assignment", () => {
-  const ctx = parseAndRun("(set x 1) x");
-
-  expectResult(ctx, 1);
+  expectEvalTo("(set x 1) x", 1);
 });
 
 describe("lte", () => {
   it("should evaluate 1 <= 2 as true", () => {
-    const ctx = parseAndRun("(lte 1 2)");
-
-    expectResult(ctx, true);
+    expectEvalTo("(lte 1 2)", true);
   });
 
   it("should evaluate 1 <= 1 as true", () => {
-    const ctx = parseAndRun("(lte 1 1)");
-
-    expectResult(ctx, true);
+    expectEvalTo("(lte 1 1)", true);
   });
   it("should evaluate 2 <= 1 as false", () => {
-    const ctx = parseAndRun("(lte 2 1)");
-
-    expectResult(ctx, false);
+    expectEvalTo("(lte 2 1)", false);
   });
   it("should require numbers", () => {
-    const ctx = parseAndRun('(lte 1 "foo")');
-
-    expectError(ctx);
+    expectEvalError('(lte 1 "foo")');
   });
   it("should require two argments", () => {
-    const ctx = parseAndRun("(lte 1 2 3)");
-
-    expectError(ctx);
+    expectEvalError("(lte 1 2 3)");
   });
 });
 
 describe("mod", () => {
   it("should evaluate 13 % 5 as 3", () => {
-    const ctx = parseAndRun("(mod 13 5)");
-
-    expectResult(ctx, 3);
+    expectEvalTo("(mod 13 5)", 3);
   });
   it("should require number arguments", () => {
-    const ctx = parseAndRun("(mod 1 null)");
-
-    expectError(ctx);
+    expectEvalError("(mod 1 null)");
   });
 });
 
 describe("equal", () => {
   it('should treat "foo" === "foo" as true', () => {
-    const ctx = parseAndRun('(equal "foo" "foo")');
-
-    expectResult(ctx, true);
+    expectEvalTo('(equal "foo" "foo")', true);
   });
   it('should treat "foo" === "bar" as false', () => {
-    const ctx = parseAndRun('(equal "foo" "bar")');
-
-    expectResult(ctx, false);
+    expectEvalTo('(equal "foo" "bar")', false);
   });
   it("should require two arguments", () => {
-    const ctx = parseAndRun('(equal "foo" "foo" "foo")');
-
-    expectError(ctx);
+    expectEvalError('(equal "foo" "foo" "foo")');
   });
 });
 
 describe("repr", () => {
   it("should wrap in double quotes", () => {
-    const ctx = parseAndRun('(repr "foo")');
-
-    expectResult(ctx, '"foo"');
+    expectEvalTo('(repr "foo")', '"foo"');
   });
 
   it("should escape double quotes", () => {
-    const ctx = parseAndRun('(repr "foo\\"bar")');
-
-    expectResult(ctx, '"foo\\"bar"');
+    expectEvalTo('(repr "foo\\"bar")', '"foo\\"bar"');
   });
 
   it("should escape alternative quotes", () => {
@@ -190,25 +152,17 @@ describe("repr", () => {
 });
 
 test("Do", () => {
-  const ctx = parseAndRun("(do 1 2)");
-
-  expectResult(ctx, 2);
+  expectEvalTo("(do 1 2)", 2);
 });
 
 test("while false", () => {
-  const ctx = parseAndRun("(while false false)");
-
-  expect(ctx.error).toBe(null);
+  expectEvalTo("(while false false)", null);
 }, 1000);
 
 test("while non-bool", () => {
-  const ctx = parseAndRun("(while 0 false)");
-
-  expect(ctx.error).not.toBe(null);
+  expectEvalError("(while 0 false)");
 }, 1000);
 
 test("while condition", () => {
-  const ctx = parseAndRun("(set x 0) (while (lte x 3) (set x (add x 1))) x");
-
-  expectResult(ctx, 4);
+  expectEvalTo("(set x 0) (while (lte x 3) (set x (add x 1))) x", 4);
 }, 1000);

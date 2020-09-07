d99b814d18fd1a0184888fad543bf551f0db1504
Wilfred Hughes <me@wilfred.me.uk>
2020-09-06 17:59:28 -0700

    Define a helper function for checking parse validity

--- src/sample-programs.test.ts
+++ src/sample-programs.test.ts
@@ -2,28 +2,29 @@ import { DEFAULT_LANG_OPTS } from "./options";
 import { buildParser } from "./parsing";
 import { helloworld, fizzbuzz, quine } from "./sample-programs";
 
-const PARSER = buildParser(DEFAULT_LANG_OPTS);
+import type { LangOpts } from "./options";
+
+function parses(src: string, opts: LangOpts): boolean {
+  const parser = buildParser(opts);
+  const result = parser.Program.parse(src);
+
+  return result.status;
+}
 
 describe("hello world", () => {
   it("should produce valid syntax with default options", () => {
-    const result = PARSER.Program.parse(helloworld(DEFAULT_LANG_OPTS));
-
-    expect(result.status).toBe(true);
+    expect(parses(helloworld(DEFAULT_LANG_OPTS), DEFAULT_LANG_OPTS));
   });
 });
 
 describe("fizzbuzz", () => {
   it("should produce valid syntax with default options", () => {
-    const result = PARSER.Program.parse(fizzbuzz(DEFAULT_LANG_OPTS));
-
-    expect(result.status).toBe(true);
+    expect(parses(fizzbuzz(DEFAULT_LANG_OPTS), DEFAULT_LANG_OPTS));
   });
 });
 
 describe("quine", () => {
   it("should produce valid syntax with default options", () => {
-    const result = PARSER.Program.parse(quine(DEFAULT_LANG_OPTS));
-
-    expect(result.status).toBe(true);
+    expect(parses(quine(DEFAULT_LANG_OPTS), DEFAULT_LANG_OPTS));
   });
 });

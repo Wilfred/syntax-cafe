3c8d7c31fc58ba7215791314ff52851863122ff9
Wilfred Hughes <me@wilfred.me.uk>
2020-09-19 17:17:06 -0700

    Refactoring expect functions for parse success/fail

--- src/parsing.test.ts
+++ src/parsing.test.ts
@@ -5,254 +5,171 @@ import { buildParser } from "./parsing";
 
 const PARSER = buildParser(DEFAULT_LANG_OPTS);
 
-function expectParseSuccess(result: P.Result<any>): void {
-  expect(result.status).toBe(true);
+function expectParseSuccess(src: string, parser: P.Language): Array<any> {
+  const result = parser.Program.parse(src);
+
+  if (!result.status) {
+    console.warn(P.formatError(src, result));
+    expect(result.status).toBe(true);
+    // Jest will have terminated on the previous line, make tsc happy.
+    throw Error("Assertion failed");
+  }
+
+  return result.value;
 }
 
-function expectParseError(result: P.Result<any>): void {
+function expectParseError(src: string, parser: P.Language): void {
+  const result = parser.Program.parse(src);
   expect(result.status).toBe(false);
 }
 
 test("Boolean literal", () => {
-  const result = PARSER.Program.parse("true");
-
-  expectParseSuccess(result);
-  if (result.status) {
-    const firstExpr = result.value[0];
-    expect(firstExpr).toMatchObject({ name: "Bool", value: true });
-  }
+  const exprs = expectParseSuccess("true", PARSER);
+  expect(exprs[0]).toMatchObject({ name: "Bool", value: true });
 });
 
 describe("String literals", () => {
   it('Should parse "foo"', () => {
-    const result = PARSER.Program.parse('"foo"');
-
-    expectParseSuccess(result);
-    if (result.status) {
-      const firstExpr = result.value[0];
-      expect(firstExpr).toMatchObject({ name: "String", value: "foo" });
-    }
+    const exprs = expectParseSuccess('"foo"', PARSER);
+    expect(exprs[0]).toMatchObject({ name: "String", value: "foo" });
   });
   it("Should parse escaped newlines", () => {
-    const result = PARSER.Program.parse('"\\n"');
+    const exprs = expectParseSuccess('"\\n"', PARSER);
 
-    expectParseSuccess(result);
-    if (result.status) {
-      const firstExpr = result.value[0];
-      expect(firstExpr).toMatchObject({ name: "String", value: "\n" });
-    }
+    expect(exprs[0]).toMatchObject({ name: "String", value: "\n" });
   });
   it("Should parse escaped doublequotes", () => {
-    const result = PARSER.Program.parse('"\\""');
+    const exprs = expectParseSuccess('"\\""', PARSER);
 
-    expectParseSuccess(result);
-    if (result.status) {
-      const firstExpr = result.value[0];
-      expect(firstExpr).toMatchObject({ name: "String", value: '"' });
-    }
+    expect(exprs[0]).toMatchObject({ name: "String", value: '"' });
   });
   it("Should parse escaped backslashes", () => {
-    const result = PARSER.Program.parse('"\\\\"');
+    const exprs = expectParseSuccess('"\\\\"', PARSER);
 
-    expectParseSuccess(result);
-    if (result.status) {
-      const firstExpr = result.value[0];
-      expect(firstExpr).toMatchObject({ name: "String", value: "\\" });
-    }
+    expect(exprs[0]).toMatchObject({ name: "String", value: "\\" });
   });
   it("Should not parse arbitrary escapes", () => {
-    const result = PARSER.Program.parse('"\\g"');
-
-    expectParseError(result);
+    expectParseError('"\\g"', PARSER);
   });
   it("Should parse qfooq if q is the delimiter", () => {
     const parser = buildParser(DEFAULT_LANG_OPTS.set("stringDelimiter", "q"));
-    const result = parser.Program.parse("qfooq");
+    const exprs = expectParseSuccess("qfooq", parser);
 
-    expectParseSuccess(result);
-    if (result.status) {
-      const firstExpr = result.value[0];
-      expect(firstExpr).toMatchObject({ name: "String", value: "foo" });
-    }
+    expect(exprs[0]).toMatchObject({ name: "String", value: "foo" });
   });
   it("Should parse |foo| if | is the delimiter", () => {
     const parser = buildParser(DEFAULT_LANG_OPTS.set("stringDelimiter", "|"));
-    const result = parser.Program.parse("|foo|");
+    const exprs = expectParseSuccess("|foo|", parser);
 
-    expectParseSuccess(result);
-    if (result.status) {
-      const firstExpr = result.value[0];
-      expect(firstExpr).toMatchObject({ name: "String", value: "foo" });
-    }
+    expect(exprs[0]).toMatchObject({ name: "String", value: "foo" });
   });
   it("Should parse escapes if | is the delimiter", () => {
     const parser = buildParser(DEFAULT_LANG_OPTS.set("stringDelimiter", "|"));
-    const result = parser.Program.parse("|foo\\|bar|");
+    const exprs = expectParseSuccess("|foo\\|bar|", parser);
 
-    expectParseSuccess(result);
-    if (result.status) {
-      const firstExpr = result.value[0];
-      expect(firstExpr).toMatchObject({ name: "String", value: "foo|bar" });
-    }
+    expect(exprs[0]).toMatchObject({ name: "String", value: "foo|bar" });
   });
 });
 
 describe("Blocks", () => {
   it("Should parse (do ... ) blocks", () => {
-    const result = PARSER.Program.parse("(do (foo) (bar))");
+    const exprs = expectParseSuccess("(do (foo) (bar))", PARSER);
 
-    expectParseSuccess(result);
-    if (result.status) {
-      const firstExpr = result.value[0];
-      expect(firstExpr).toMatchObject({ name: "Block" });
-    }
+    expect(exprs[0]).toMatchObject({ name: "Block" });
   });
   it("Should parse { ... } curly blocks", () => {
     const parser = buildParser(
       DEFAULT_LANG_OPTS.set("statementTerminator", ";")
     );
-    const src = "{ (foo); (bar); }";
-    const result = parser.Program.parse(src);
+    const exprs = expectParseSuccess("{ (foo); (bar); }", parser);
 
-    if (!result.status) {
-      console.warn(P.formatError(src, result));
-    }
-
-    expectParseSuccess(result);
-    if (result.status) {
-      const firstExpr = result.value[0];
-      expect(firstExpr).toMatchObject({ name: "Block" });
-    }
+    expect(exprs[0]).toMatchObject({ name: "Block" });
   });
 });
 
 test("Trailing whitespace should be permitted", () => {
-  const result = PARSER.Program.parse("true \n \n");
-
-  expectParseSuccess(result);
+  expectParseSuccess("true \n \n", PARSER);
 });
 
 test("Empty list should be a parse error", () => {
-  const result = PARSER.Program.parse("()");
-  expectParseError(result);
+  expectParseError("()", PARSER);
 });
 
 test("Parse a simple list", () => {
-  const result = PARSER.Program.parse("(foo true)");
-  expectParseSuccess(result);
+  expectParseSuccess("(foo true)", PARSER);
 });
 
 test("Whitespace inside list", () => {
-  const result = PARSER.Program.parse("(foo )");
-  expectParseSuccess(result);
+  expectParseSuccess("(foo )", PARSER);
 });
 
 test("Comments with ( should take precedence", () => {
   const parser = buildParser(DEFAULT_LANG_OPTS.set("commentPrefix", "("));
-  const result = parser.Program.parse("(foo true)\n(foo bar)");
+  const exprs = expectParseSuccess("(foo true)\n(foo bar)", parser);
 
-  expectParseSuccess(result);
-  if (result.status) {
-    expect(result.value).toStrictEqual([]);
-  }
+  expect(exprs).toStrictEqual([]);
 });
 
 describe("if", () => {
   it("should parse (if ...)", () => {
-    const result = PARSER.Program.parse("(if true 1 2)");
+    const exprs = expectParseSuccess("(if true 1 2)", PARSER);
 
-    expectParseSuccess(result);
-    if (result.status) {
-      const firstExpr = result.value[0];
-      expect(firstExpr).toMatchObject({ name: "If" });
-    }
+    expect(exprs[0]).toMatchObject({ name: "If" });
   });
   it("should parse (customkeyword ...)", () => {
     const parser = buildParser(DEFAULT_LANG_OPTS.set("ifKeyword", "custom"));
-    const result = parser.Program.parse("(custom true 1 2)");
+    const exprs = expectParseSuccess("(custom true 1 2)", parser);
 
-    expectParseSuccess(result);
-    if (result.status) {
-      const firstExpr = result.value[0];
-      expect(firstExpr).toMatchObject({ name: "If" });
-    }
+    expect(exprs[0]).toMatchObject({ name: "If" });
   });
   it("should parse if {...} else {...}", () => {
     const parser = buildParser(
       DEFAULT_LANG_OPTS.set("statementTerminator", ";")
     );
-    const src = "if true { (foo); } else { (bar); }";
-    const result = parser.Program.parse(src);
+    const exprs = expectParseSuccess(
+      "if true { (foo); } else { (bar); }",
+      parser
+    );
 
-    if (!result.status) {
-      console.warn(P.formatError(src, result));
-    }
-    expectParseSuccess(result);
-    if (result.status) {
-      const firstExpr = result.value[0];
-      expect(firstExpr).toMatchObject({ name: "If" });
-    }
+    expect(exprs[0]).toMatchObject({ name: "If" });
   });
 });
 
 describe("while", () => {
   it("should parse (if ...)", () => {
-    const result = PARSER.Program.parse("(while true 2)");
+    const exprs = expectParseSuccess("(while true 2)", PARSER);
 
-    expectParseSuccess(result);
-    if (result.status) {
-      const firstExpr = result.value[0];
-      expect(firstExpr).toMatchObject({ name: "While" });
-    }
+    expect(exprs[0]).toMatchObject({ name: "While" });
   });
 
   it("should parse while true {}", () => {
     const parser = buildParser(
       DEFAULT_LANG_OPTS.set("statementTerminator", ";")
     );
-    const src = "while true {}";
-    const result = parser.Program.parse(src);
+    const exprs = expectParseSuccess("while true {}", parser);
 
-    if (!result.status) {
-      console.warn(P.formatError(src, result));
-    }
-    expectParseSuccess(result);
-    if (result.status) {
-      const firstExpr = result.value[0];
-      expect(firstExpr).toMatchObject({ name: "While" });
-    }
+    expect(exprs[0]).toMatchObject({ name: "While" });
   });
 });
 
 describe("Assign", () => {
   it("should parse valid assignments", () => {
-    const result = PARSER.Program.parse("(set x 1)");
+    const exprs = expectParseSuccess("(set x 1)", PARSER);
 
-    expectParseSuccess(result);
-    if (result.status) {
-      const firstExpr = result.value[0];
-      expect(firstExpr).toMatchObject({ name: "Assign" });
-    }
+    expect(exprs[0]).toMatchObject({ name: "Assign" });
   });
   it("should reject assigning to non-symbols", () => {
-    const result = PARSER.Program.parse("(set 1 1)");
-    expectParseError(result);
+    expectParseError("(set 1 1)", PARSER);
   });
   it("should reject assigning to boolean constants", () => {
-    const result = PARSER.Program.parse("(set true 1)");
-    expectParseError(result);
+    expectParseError("(set true 1)", PARSER);
   });
 
   it("should use infix = with statements", () => {
     const parser = buildParser(
       DEFAULT_LANG_OPTS.set("statementTerminator", ";")
     );
-    const src = "x = 1;";
-    const result = parser.Program.parse(src);
-    expectParseSuccess(result);
-    if (result.status) {
-      const firstExpr = result.value[0];
-      expect(firstExpr).toMatchObject({ name: "Assign" });
-    }
+    const exprs = expectParseSuccess("x = 1;", parser);
+    expect(exprs[0]).toMatchObject({ name: "Assign" });
   });
 });
